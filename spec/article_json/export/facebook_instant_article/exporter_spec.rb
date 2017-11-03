904f0cd668173644d73821ef3dce283d0dab0449
Nicolas Fricke <nicolas.fricke@devex.com>
2017-11-03 18:44:45 +0100

    Add basic structure for a Facebook Instant Article exporter
    This currently is just a copy of the HTML exporter as Facebook Instant
    Articles are simply a limited and a bit customized version of HTML.
    
    Further commits will then include the FBIA custom elements, like custom
    classes, different tag names or special nesting.
    
    For more information on the Facebook Instant Article HTML format, please
    refer to: https://developers.facebook.com/docs/instant-articles/guides/format-overview

---
+++ spec/article_json/export/facebook_instant_article/exporter_spec.rb
@@ -0,0 +1,18 @@
+describe ArticleJSON::Export::FacebookInstantArticle::Exporter do
+  subject(:exporter) { described_class.new(elements) }
+
+  describe 'reference document test' do
+    subject { exporter.html }
+    let(:html) { File.read('spec/fixtures/reference_document_exported.html') }
+    let(:json) { File.read('spec/fixtures/reference_document_parsed.json') }
+    let(:elements) { ArticleJSON::Article.from_json(json).elements }
+    before { stub_oembed_requests }
+    it { should eq html.strip }
+  end
+
+  describe '.namespace' do
+    subject { described_class.namespace }
+    it { should be_a Module }
+    it { should eq ArticleJSON::Export::FacebookInstantArticle }
+  end
+end
