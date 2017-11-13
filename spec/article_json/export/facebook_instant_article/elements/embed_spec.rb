bd178fdd0fe00087d21a7ca2ebe343cd5bebfa0a
Manu Campos <manu@invisibles.cc>
2017-11-13 15:55:20 +0100

    DVX-6275 Export Embedded elements for Facebook Instant Articles
    Export embedded elements (tweets, youtube videos, etc) for Instant Articles
    following the reference document:
    https://developers.facebook.com/docs/instant-articles/reference/embeds

--- spec/article_json/export/facebook_instant_article/elements/embed_spec.rb
+++ spec/article_json/export/facebook_instant_article/elements/embed_spec.rb
@@ -1,9 +1,10 @@
 describe ArticleJSON::Export::FacebookInstantArticle::Elements::Embed do
   subject(:element) { described_class.new(source_element) }
+  let(:type) { :youtube_video }
 
   let(:source_element) do
     ArticleJSON::Elements::Embed.new(
-      embed_type: :something,
+      embed_type: type,
       embed_id: 666,
       caption: [ArticleJSON::Elements::Text.new(content: 'Foo Bar')],
       tags: %w(test)
@@ -12,14 +13,40 @@ describe ArticleJSON::Export::FacebookInstantArticle::Elements::Embed do
 
   describe '#export' do
     subject { element.export.to_html(save_with: 0) }
-    let(:expected_html) do
-      '<figure><div class="embed">Embedded Object: something-666</div>' \
-        '<figcaption>Foo Bar</figcaption></figure>'
-    end
     let(:oembed_data) { { html: 'Embedded Object: something-666' } }
     before do
       allow(source_element).to receive(:oembed_data).and_return(oembed_data)
     end
-    it { should eq expected_html }
+
+    context 'when the element it is not a facebook vide nor a tweet' do
+      let(:expected_html) do
+        '<figure class="op-interactive">Embedded Object: something-666' \
+        '<figcaption>Foo Bar</figcaption></figure>'
+      end
+
+      it { should eq expected_html }
+    end
+
+    context 'with a tweet' do
+      let(:type) { :tweet }
+      let(:expected_html) do
+        '<figure class="op-interactive">' \
+        '<iframe>Embedded Object: something-666</iframe>' \
+        '<figcaption>Foo Bar</figcaption></figure>'
+      end
+
+      it { should eq expected_html }
+    end
+
+    context 'with a tweet' do
+      let(:type) { :facebook_video }
+      let(:expected_html) do
+        '<figure class="op-interactive">' \
+        '<iframe>Embedded Object: something-666</iframe>' \
+        '<figcaption>Foo Bar</figcaption></figure>'
+      end
+
+      it { should eq expected_html }
+    end
   end
 end
